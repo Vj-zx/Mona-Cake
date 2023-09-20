53a1eee586d3f22cf4fc4ddad63e753e87912682
Vj-zx <145135841+Vj-zx@users.noreply.github.com>
2023-09-20 21:26:11 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenELements = document.querySelectorAll('.hidden');
+hiddenELements.forEach((el) => observer.observe(el));
